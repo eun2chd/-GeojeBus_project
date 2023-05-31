3a1cc6c2ebf143f33f69057b83d0ea16185d4143
echong <tdc8165@naver.com>
2023-05-31 23:33:06 +0900

    노드 공부가 필요함

--- myBus1.1/js/index.js
+++ myBus1.1/js/index.js
@@ -1,26 +1,37 @@
-let request = require('request');
-let cheerio = require('cheerio');
-
- // REST API 경로
-  const $url = 'http://data.geoje.go.kr/rfcapi/rest/geojebis/getGeojebisBusline';
- // api key
-  const $key = '13BNQreTIhd7rEK1zkf4AmYxzLq1aOAf2aHtwpBMBApzgarsPRMjp0PvWyRjUBtAn7BOGPiJ21USVhA62wNANA==';
-  pageSize = '10';
-
-// const $url = 'http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll';
-// const $key =  '13BNQreTIhd7rEK1zkf4AmYxzLq1aOAf2aHtwpBMBApzgarsPRMjp0PvWyRjUBtAn7BOGPiJ21USVhA62wNANA==';
-const $busid = '100100118';
-const $api_url = $url + '?authApiKey='+ $key + '&pageSize=' + pageSize;
-console.log($api_url);
+ let request = require('request');
+ let cheerio = require('cheerio');
 
 
-request($api_url, function(err,res,body) {
-    $ = cheerio.load(body);
 
-    $('list').each(function(idx) {
-        let no1 = $(this).find('dir_up_name').text();
-        console.log(`test : ${no1}`);
-    });
-});
+  // REST API 경로
+   const $url = 'http://data.geoje.go.kr/rfcapi/rest/geojebis/getGeojebisBusarrive';
+  // api key
+   const $key = '13BNQreTIhd7rEK1zkf4AmYxzLq1aOAf2aHtwpBMBApzgarsPRMjp0PvWyRjUBtAn7BOGPiJ21USVhA62wNANA%3D%3D';
+ //   pageSize = '10';
+
+ const $line_name = '16-3';
+ const $api_url = $url + '?authApiKey='+ $key + '&line_name=' + $line_name;
+ console.log($api_url);
+
+
+ request($api_url, function(err,res,body) {
+      var $ = cheerio.load(body, {xmlMode : true});
+
+      $('list').each(function(idx) {
+          let num = $(this).find('num').text();
+          let busline = $(this).find('line_name').text();
+          let busstop_name = $(this).find('busstop_name').text();
+          let ars_id = $(this).find('ars_id').text();
+          console.log(`순번 : ${num}`);
+          console.log(`노선번호 : ${busline}`);
+          console.log(`현재정류장 : ${busstop_name}`);
+          console.log(`정류장 번호 : ${ars_id}`);
+          console.log('----------------------------')
+     });
+ });
+
+
+
+
 
 
